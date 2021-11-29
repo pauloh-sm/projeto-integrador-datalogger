696815a37a41abe5ff046a84715d7769b8035030
Paulo Henrique Schulz <pauloh.sm@hotmail.com>
2021-11-29 02:31:21 -0300

    Ajuste no conteudo do arquivo

--- projeto-integrador-datalogger.ino
+++ projeto-integrador-datalogger.ino
@@ -1,6 +1,6 @@
 #include <SPI.h>
 #include <SD.h>
-#include <DS1307.h>
+//#include <DS1307.h>
 
 #define pinChipSelectSD 4
 
@@ -12,38 +12,44 @@ enum sensorType {
   Temperature = 3
 } sensorsType;
 
-DS1307 rtc(A4, A5);
+//DS1307 rtc(A4, A5);
 
 void setup() {
   startConfigure();
   sdConfigure();
-  rtcConfigure();
+//  rtcConfigure();
 }
 
 void loop() {
-  dataLogger = fileOpen();
+  fileOpen();
   
   if (dataLogger) {
     String dataRead = serialRead();
 
     if(dataRead.length() > 0){
       readData(dataRead);
-    }    
+    }
+    dataLogger.close();
   }
-
-  fileClose(dataLogger);
 }
 
-void rtcConfigure() {
-  rtc.halt(false);
-  rtc.setSQWRate(SQW_RATE_1);
-  rtc.enableSQW(true);
-}
+//void rtcConfigure() {
+//  rtc.halt(false);
+//  rtc.setSQWRate(SQW_RATE_1);
+//  rtc.enableSQW(true);
+//}
 
 void sdConfigure() {
+  Serial.println("Iniciando SD");
+
+  delay(1000);
+  
   if (!SD.begin(pinChipSelectSD)) {
+    Serial.println("Erro ao iniciar SD");
     while (1);
   }
+
+  Serial.println("SD iniciado");
 }
 
 void startConfigure() {
@@ -51,39 +57,58 @@ void startConfigure() {
 }
 
 void loggerFuelSensor(String dataFuel) {
-  String fuelSensorData = getDateTimeNow()+";"+dataFuel;
+//  String fuelSensorData = getDateTimeNow()+";"+dataFuel;
+  String fuelSensorData = String(FuelSensor)+";"+dataFuel;
   dataLogger.println(fuelSensorData);
+
+  Serial.print("Record: ");
+  Serial.println(fuelSensorData);
 }
 
-void loggerSpeedometerSensor(File dataLogger) {
-//  byte fuelSpeedometerState = digitalRead(pinSpeedometerSensor);
-//  String fuelSensorData = getDateTimeNow()+";"+String(Speedometer)+";"+String(fuelSensorState);
-//  dataLogger.println(fuelSensorData);
+void fileOpen() {
+  dataLogger = SD.open("baja.csv", FILE_WRITE);
+
+  if (dataLogger) {
+    
+  } else {
+    Serial.println("Error opening...");
+  }
 }
 
-File fileOpen() {
-  return SD.open("baja_data_logger.csv", FILE_WRITE);
-}
-
-void fileClose(File dataLogger) {
-  dataLogger.close();
-}
-
-String getDateTimeNow() {
-  return String(rtc.getDateStr(FORMAT_SHORT))+"T"+String(rtc.getTimeStr());
-}
+//String getDateTimeNow() {
+//  return String(rtc.getDateStr(FORMAT_SHORT))+"T"+String(rtc.getTimeStr());
+//}
 
 String serialRead() {
   String incomingData = "";
   if (Serial.available() > 0) {
-    incomingData = Serial.readString();    
+    incomingData = Serial.readString();   
+    Serial.print("incomingData: ");
+    Serial.println(incomingData); 
   }
-
   return incomingData;
 }
 
 void readData(String data){
-  if(data[0] == (char)FuelSensor){
-    loggerFuelSensor(data);
+  Serial.print("ReadData: ");
+  Serial.println(data);
+
+  Serial.print("SensorType: ");
+  Serial.println(data[0]);
+  
+  if(String(data[0]) == String(FuelSensor)){
+    loggerFuelSensor(returnData(data));
   }
+  else {
+    Serial.println("Sensor Type Not Found!!");
+  }
+}
+
+String returnData(String completeData){
+  String justData = "";
+  for(int i = 1; i<completeData.length(); i++){
+    if(completeData[i] != '\r' && completeData[i] != '\n')
+      justData += String(completeData[i]);
+  }
+  return justData;
 }
